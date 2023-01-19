526006658f737ba92ef5ef9a96a692da36ff8c28
Marvin Xiquitá Xajpot <Marvin.Xiquita.Xajpot@gt.ey.com>
2023-01-19 10:29:45 -0600

    updating actors

--- controllers/actor.js
+++ controllers/actor.js
@@ -1,5 +1,7 @@
+const { isValidObjectId } = require("mongoose");
 const Actor = require("../models/actor");
 const cloudinary = require('cloudinary').v2;
+const { sendError } = require("../utils/helper")
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -13,16 +15,76 @@ exports.createActor = async (req, res) => {
     const { file } = req;
 
     const newActor = new Actor({ name, about, gender });
-    const {secure_url, public_id} = await cloudinary.uploader.upload(file.path)
 
-    newActor.avatar = {
-        url: secure_url,
-        public_id
+    if (file) {
+        const { secure_url, public_id } = await cloudinary.uploader.upload(file.path)
+        newActor.avatar = {
+            url: secure_url,
+            public_id
+        }
     }
 
     await newActor.save();
 
     res.status(201).json({
-        actor: newActor
+        actor: {
+            id: newActor._id,
+            name,
+            about,
+            gender,
+            avatar: newActor.avatar?.url
+        }
+    })
+}
+
+
+exports.updateActor = async (req, res) => {
+    const { name, about, gender } = req.body;
+    const { file } = req;
+    const { id } = req.params;
+
+    if (!isValidObjectId(id)) {
+        return sendError(res, 'Invalid request!')
+    }
+
+    const actor = await Actor.findById(id);
+    if (!actor) {
+        return sendError(res, 'Invalid request, record not found!', 404);
+    }
+
+    const public_id = actor.avatar?.public_id;
+
+    if (public_id && file) {
+        const { result } = await cloudinary.uploader.destroy(public_id);
+        if (result !== 'ok') {
+            return reusendError(res, 'Could not remove image from cloud!')
+        }
+        console.log(result);
+    }
+
+    // upload avatar
+    if (file) {
+        const { secure_url, public_id } = await cloudinary.uploader.upload(file.path)
+        actor.avatar = {
+            url: secure_url,
+            public_id
+        }
+    }
+
+    actor.name = name;
+    actor.about = about;
+    actor.gender = gender;
+
+
+    await actor.save();
+
+    res.status(201).json({
+        actor: {
+            id: actor._id,
+            name,
+            about,
+            gender,
+            avatar: actor.avatar?.url
+        }
     })
 }
